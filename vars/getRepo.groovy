ceaafc820e93b89d1ae7773afdd3df5c2dad0b62
Krishna Pavan <77038420+krishnapavan630@users.noreply.github.com>
2026-05-26 00:51:10 +0530

    Fix variable name in echo statement

--- vars/getRepo.groovy
+++ vars/getRepo.groovy
@@ -1,5 +1,5 @@
 def call(String addr, String branchName){
-    echo "Trying to clone the ${addr} from ${branch}"
+    echo "Trying to clone the ${addr} from ${branchName}"
     git url: addr, branch: branchName
     echo "Checkout completed"
 }
